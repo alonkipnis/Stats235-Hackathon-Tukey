0912fe11abfed7718a962a0bda9bf6570a9fb2bb
Enze Chen <enze.chen1@gmail.com>
2018-11-17 15:46:20 -0800

    Incremental changes; merge master

--- Code/tukey_exp.py
+++ Code/tukey_exp.py
@@ -1,17 +1,18 @@
 import os, sys, re, time, csv
 import numpy as np 
 import pandas as pd
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 from word_lists import *
 from two_unit_test import two_unit_test
 
 
-infile = '~/Data/speech_w_data_example.csv'
-interval = 1
-outfile = '~/Data/results_{}month.csv'.format(interval)
-vocab_csv = '~/Data/list_of_1500words.csv'
-fieldnames = ["Interval", "Date1", "Party1", "Affil1", "Date2", "Party2", "Affil2", "HC_score", "Features"]
+# infile = '~/Data/speech_w_data_example.csv'
+infile = '../Data/speech_w_data_example.csv'
+# vocab_csv = '~/Data/list_of_1500words.csv'
+vocab_csv = '../Data/list_of_1500words.csv'
+resnames = ["Interval", "Date1", "Party1", "Affil1", "Date2", "Party2", "Affil2", "HC_score", "Features"]
+datanames = ['speech_id', 'date', 'congress_id', 'chamber', 'party', 'speech']
 
 
 # Calculates the line breaks for each month of data
@@ -37,7 +38,7 @@ def calculate_line_breaks(infile):
 
 
 # Run an experiment with a set of parameters
-def run_experiment(interval, unit1, unit2, parties, chambers, vocab_list, ignore_list):
+def run_experiment(outfile, interval, unit1, unit2, parties, chambers, vocab_list, ignore_list):
     comp_unit1 = unit1.loc[(unit1.party == parties[0]) & (unit1.chamber == chambers[0]), ['speech_id', 'speech']]
     comp_unit2 = unit2.loc[(unit2.party == parties[1]) & (unit2.chamber == chambers[1]), ['speech_id', 'speech']]
     hc, features = two_unit_test(comp_unit1, comp_unit2, vocab_list)
@@ -51,24 +52,13 @@ def run_experiment(interval, unit1, unit2, parties, chambers, vocab_list, ignore
         print("Results written to CSV")
 
 
-
-# Analyze the results by plotting HC over time
-def hc_analysis(outfile):
-    df = pd.read_csv(outfile)
-    fig, ax = plt.subplots()
-    fig2 = plt.plot(df.Interval, df.HC_score, 'bo')
-    plt.xlabel('Days Apart')
-    plt.ylabel('HC score')
-    plt.savefig('../Data/HC_vs_time_interval.png')
-
-
-
 # Calls sequence of experiments
 def main():
     # Experimental parameters / setup
     a = time.time()
     line_breaks, dates = calculate_line_breaks(infile)
-    intervals = [1, 6, 12]
+    numunits = len(dates)
+    intervals = [1]
     parties = ["D", "R"]
     chambers = ["H", "H"]
     congress_id = 114
@@ -77,32 +67,24 @@ def main():
     ignore_list = words_to_ignore + function_words + additional_words1 + additional_words2 + singletons
     b = time.time()
     print("There are {} units to compare".format(len(dates)))
-    with open(outfile, 'w', newline = '') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(fieldnames)
     print("Time for setup is {0:.3f} seconds".format(b - a))
 
 
-    # Calculate units
-    numunits = len(dates)
-    for i in range(numunits-4, numunits, interval):
-        for j in range(i + interval, numunits, interval):
-            a = time.time()
-            unit1 = pd.read_csv(infile, encoding = 'latin1', skiprows = line_breaks[i], nrows = line_breaks[i+interval] - line_breaks[i], names = ['speech_id', 'date', 'congress_id', 'chamber', 'party', 'speech'])
-            unit2 = 0
-            if j >= numunits - interval:
-                unit2 = pd.read_csv(infile, encoding = 'latin1', skiprows = line_breaks[j], names = ['speech_id', 'date', 'congress_id', 'chamber', 'party', 'speech'])
-            else:
-                unit2 = pd.read_csv(infile, encoding = 'latin1', skiprows = line_breaks[j], nrows = line_breaks[j+interval] - line_breaks[j], names = ['speech_id', 'date', 'congress_id', 'chamber', 'party', 'speech'])
-            print("Comparing units in {} and {}...".format(dates[i], dates[j]))
-            run_experiment(j - i, unit1, unit2, parties, chambers, vocab_list, ignore_list)
-            b = time.time()
-            print("Time for running 1 iteration is {0:.3f} seconds".format(b - a))
-
-
-    # Analyze results
-    hc_analysis(outfile)
-
+    # Iterate through units
+    for interval in intervals:
+        outfile = '../Data/results_{}month.csv'.format(interval)
+        with open(outfile, 'w', newline = '') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(resnames)
+        for i in range(0, numunits, interval):
+            for j in range(i + interval, numunits - interval):
+                a = time.time()
+                unit1 = pd.read_csv(infile, encoding = 'latin1', skiprows = line_breaks[i], nrows = line_breaks[i+interval] - line_breaks[i], names = datanames)
+                unit2 = pd.read_csv(infile, encoding = 'latin1', skiprows = line_breaks[j], nrows = line_breaks[j+interval] - line_breaks[j], names = datanames)
+                print("Comparing units in {} and {}...".format(dates[i], dates[j]))
+                run_experiment(outfile, j - i, unit1, unit2, parties, chambers, vocab_list, ignore_list)
+                b = time.time()
+                print("Time for running 1 iteration is {0:.3f} seconds".format(b - a))
 
 
 if __name__ == '__main__':
