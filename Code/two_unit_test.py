9aa5512f6afd567e6cf7c41913c67df216d4b438
alonkipnis@gmail.com <alonkipnis@gmail.com>
2018-11-17 11:58:44 -0800

    update two.unit test and exmaple_python

--- Code/two_unit_test.py
+++ Code/two_unit_test.py
@@ -1,26 +1,33 @@
 
 import pandas as pd
 import numpy as np
-from nltk.stem.snowball import SnowballStemmer
+from HC_aux import hc_vals
+
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+
+import re
+from nltk import word_tokenize          
+from nltk.stem import SnowballStemmer 
+class LemmaTokenizer(object):
+     def __init__(self):
+         self.wnl = SnowballStemmer(language = 'english')
+     def __call__(self, doc):
+        doc = re.sub(r'[^A-Za-z0-9\s]',r' ',doc)
+        doc = re.sub(r'\n',r' ',doc)
+        doc = re.sub(r'[0-9]',r' ',doc)
+        #doc = re.sub(r'[a-z]\040' ,r'',doc) #remove singletons
+        return [self.wnl.stem(t) for t in word_tokenize(doc)]
+
 import scipy.stats as stats
 from HC_aux import hc_vals
 
 
-stemmer = SnowballStemmer("english")
-
 
 #Some helper functions for two_unit_test function
 
-def get_p(freq_x,freq_y,total_x,total_y):
+def get_z_score(freq_x,freq_y,total_x,total_y):
     p = (freq_x + freq_y)/np.float((total_x + total_y))
-    return p
-
-def get_se(p, total_x,total_y):
     se = np.sqrt(p * (1.0-p) * (1.0/total_x + 1.0/total_y))
-    return se
-
-
-def get_z_score(freq_x,freq_y,total_x,total_y,se):
     z_score = (freq_x /np.float(total_x) - freq_y/np.float(total_y))/np.float(se)
     return z_score
 
@@ -34,72 +41,33 @@ def get_pval2(freq_x, freq_y,total_x,total_y):
     return pval2
 
 
+
+def two_unit_test(unit1,unit2, list_of_words):
 # Input: unit1, unit2, which are dataframes with columns: speech_id (integer) , speech (string)
-# Input: context_words, non_context_words; these lists of words we will keep
-# Output: a dataframe containing hc statistic, a boolean array features which is 1 if the feature was important,
-#         and an array of corresponding words which were used for the HC evaluation
+# Input: lists_of_words = features
+# Output: hc score, list of distinguishing features
 
-def two_unit_test(unit1,unit2, context_words,non_context_words):
-
-    #Operations on unit1
-    unit1 = unit1.drop('speech_id', axis = 1)
-    unit1_words = pd.DataFrame(unit1.speech.str.split().tolist()).stack()
-    unit1_words = unit1_words.reset_index()[[0]] #Keep the column 0, corresponding to 'word'
-    unit1_words.columns = ['word']
-    unit1_words['word'] = unit1_words['word'].str.extract(r'([a-zA-z]+)')
-    unit1_words = unit1_words.dropna()
-    unit1_words['word'] = unit1_words['word'].apply(stemmer.stem)
-    unit1_words = unit1_words.apply(pd.value_counts) #after this, words become the index column, and the frequency becomes the word column
-    unit1_words = unit1_words.reset_index()[['index', 'word']] 
-    unit1_words['freq'] = unit1_words['word']
-    unit1_words['word'] = unit1_words['index']
-    unit1_words = unit1_words.drop('index', axis = 1)
-    unit1_words['total'] = unit1_words['freq'].sum()
-
-    #Same operations on unit2
-    unit2 = unit2.drop('speech_id', axis = 1)
-    unit2_words = pd.DataFrame(unit2.speech.str.split().tolist()).stack()
-    unit2_words = unit2_words.reset_index()[[0]] #Keep the column 0, corresponding to 'word'
-    unit2_words.columns = ['word']
-
-    unit2_words['word'] = unit2_words['word'].str.extract(r'([a-zA-z]+)')
-    unit2_words = unit2_words.dropna()
-    unit2_words['word'] = unit2_words['word'].apply(stemmer.stem)
-    unit2_words = unit2_words.apply(pd.value_counts) #after this, words become the index column, and the frequency becomes the word column
-    unit2_words = unit2_words.reset_index()[['index', 'word']] 
-    unit2_words['freq'] = unit2_words['word']
-    unit2_words['word'] = unit2_words['index']
-    unit2_words = unit2_words.drop('index', axis = 1)
-    unit2_words['total'] = unit2_words['freq'].sum()
+    tf_vectorizer = CountVectorizer(tokenizer=LemmaTokenizer(), vocabulary=list_of_words)
+    tf1 = tf_vectorizer.fit_transform(unit1['speech'])
+    tf2 = tf_vectorizer.fit_transform(unit2['speech'])
+  
+    word_counts = pd.DataFrame()
+    word_counts['word'] = (tf_vectorizer.get_feature_names())
+    word_counts['n1'] = np.array(tf1.sum(0)).T
+    word_counts['n2'] = np.array(tf2.sum(0)).T
+    word_counts['T1'] = word_counts['n1'].sum()
+    word_counts['T2'] = word_counts['n2'].sum()
     
     #Joining unit1 and unit2 for the HC computation
-
-    word_counts = pd.merge(unit1_words,unit2_words,on = 'word')
-    word_counts = word_counts[word_counts['word'].isin(context_words + non_context_words)]
-    word_counts['total'] = word_counts['total_x'] + word_counts['total_y']
-    word_counts = word_counts[word_counts['freq_x'] + word_counts['freq_y'] > 1] #double-check this line
-
-    word_counts['p'] = word_counts.apply(lambda row: get_p(row['freq_x'], 
-                                                         row['freq_y'], 
-                                                         row['total_x'],
-                                                        row['total_y']), axis=1)
-    word_counts['se'] = word_counts.apply(lambda row: get_se(row['p'], 
-                                                         row['total_x'],
-                                                        row['total_y']), axis=1)
-    word_counts['z_score'] = word_counts.apply(lambda row: get_z_score(row['freq_x'],
-                                                                 row['freq_y'],
-                                                                 row['total_x'],
-                                                                 row['total_y'],
-                                                                 row['se']), axis = 1)
-    word_counts['pval'] = word_counts.apply(lambda row: get_pval(row['z_score']),axis = 1)
-    word_counts['pval2'] = word_counts.apply(lambda row: get_pval2(row['freq_x'], 
-                                                         row['freq_y'], 
-                                                         row['total_x'],
-                                                        row['total_y']), axis=1)
+    word_counts = word_counts[word_counts['n1'] + word_counts['n2'] >= 10] 
+    word_counts['pval'] = word_counts.apply(lambda row: get_pval2(row['n1'], 
+                                                         row['n2'], 
+                                                         row['T1'],
+                                                        row['T2']), axis=1)
     
     #Pass in pval2, from binomial test, into HC function
-    hc_result = hc_vals(word_counts['pval2'], alpha = 0.4)
-    features = np.where(word_counts['pval2'] <= hc_result.hc, 1, 0) 
-    return pd.DataFrame({'hc': hc_result.hc, 'features': features, 'word': word_counts['word']})
+    hc_result = hc_vals(word_counts['pval'], alpha = 0.4)
+    features = hc_result.p_sorted_idx[:hc_result.i_max_star]
+    return hc_result.hc, features
 
 
