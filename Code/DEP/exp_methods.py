8efa710c45973e7a447767fd543bad310082d2d8
Enze Chen <enze.chen1@gmail.com>
2018-11-18 00:38:28 -0800

    Started topics tests

--- Code/DEP/exp_methods.py
+++ Code/DEP/exp_methods.py
@@ -6,14 +6,18 @@ from two_unit_test import two_unit_test
 
 
 speech_count_thresh = 300
-cluster = 1
+on_cluster = False
 infile, vocab_csv = None, None
-if cluster:
+if on_cluster:
     infile = os.path.expanduser('~/Data/speech_w_data.csv')
     vocab_csv = os.path.expanduser('~/Data/list_of_1500words.csv')
+    topic_25_csv = os.path.expanduser('~/Data/word_25topics_LDA.csv')
+    topic_75_csv = os.path.expanduser('~/Data/word_25topics_LDA.csv')
 else:
     infile = '../Data/speech_w_data.csv'
     vocab_csv = '../Data/list_of_1500words.csv'
+    topic_25_csv = '../Data/word_25topics_LDA.csv'
+    topic_75_csv = '../Data/word_75topics_LDA.csv'
 
 resnames = ["Interval", "Date1", "Party1", "Affil1", "Date2", "Party2", "Affil2", "HC_score", "Features"]
 datanames = ['speech_id', 'date', 'congress_id', 'chamber', 'party', 'speech']
@@ -44,23 +48,33 @@ def calculate_line_breaks(infile, skip_lines):
 
 
 # Make parameters to loop through
-def build_params(intervals, nummonths):
+def build_params(intervals, nummonths, topic_lists = None):
     params_list = []
-    for interval in intervals:
-        for i in range(0, nummonths - 2 * interval, interval):
-            params_list.append([interval, i, ['N', 'N']])
-            params_list.append([interval, i, ['D', 'D']])
-            params_list.append([interval, i, ['D', 'R']])
-            params_list.append([interval, i, ['R', 'D']])
-            params_list.append([interval, i, ['R', 'R']])
+    if topic_lists is None:
+        for interval in intervals:
+            for i in range(0, nummonths - 2 * interval, interval):
+                params_list.append([interval, i, ['N', 'N']])
+                params_list.append([interval, i, ['D', 'D']])
+                params_list.append([interval, i, ['D', 'R']])
+                params_list.append([interval, i, ['R', 'D']])
+                params_list.append([interval, i, ['R', 'R']])
+    else:
+        for interval in intervals:
+            for i in range(0, nummonths - 2 * interval, interval):
+                for topic in topic_lists:
+                    params_list.append([interval, i, ['N', 'N'], topic])
+                    params_list.append([interval, i, ['D', 'D'], topic])
+                    params_list.append([interval, i, ['D', 'R'], topic])
+                    params_list.append([interval, i, ['R', 'D'], topic])
+                    params_list.append([interval, i, ['R', 'R'], topic])
     return params_list
 
 
 
 # Run an experiment with a set of parameters
-def run_experiment(infile, interval, i, line_breaks, dates, vocab_list, parties):
+def run_text_experiment(infile, interval, i, line_breaks, dates, vocab_list, parties):
     for j in range(i + interval, len(dates) - interval, interval):
-        print("Comparing units in {} and {} between {}...".format(dates[i], dates[j], parties))
+        print("Comparing text in units {} and {} between {} parties...".format(dates[i], dates[j], parties))
         unit1 = pd.read_csv(infile, encoding = 'latin1', skiprows = line_breaks[i], nrows = line_breaks[i+interval] - line_breaks[i], names = datanames)
         unit2 = pd.read_csv(infile, encoding = 'latin1', skiprows = line_breaks[j], nrows = line_breaks[j+interval] - line_breaks[j], names = datanames)
 
@@ -88,6 +102,45 @@ def run_experiment(infile, interval, i, line_breaks, dates, vocab_list, parties)
             outfile = os.path.expanduser('~/Data/results_{}_{}_{}.csv'.format(interval, parties[0], parties[1]))
         else:
             outfile = '../Data/results_{}_{}_{}.csv'.format(interval, parties[0], parties[1])
+        unit_dates = [str(unit1.date[2])[:6], str(unit2.date[2])[:6]]
+        with open(outfile, 'a', newline = '') as csvfile:
+            writer = csv.writer(csvfile)
+            line = [j - i, unit_dates[0], parties[0], dates[1], parties[1], hc, ','.join([str(f) for f in features])]
+            writer.writerow(line)
+
+
+
+# Run an experiment with NN clustering for topic assignment
+def run_topic_experiment(infile, interval, i, line_breaks, dates, topic_list, parties):
+    for j in range(i + interval, len(dates) - interval, interval):
+        print("Comparing topic in units {} and {} between {} parties...".format(dates[i], dates[j], parties))
+        unit1 = pd.read_csv(infile, encoding = 'latin1', skiprows = line_breaks[i], nrows = line_breaks[i+interval] - line_breaks[i], names = datanames)
+        unit2 = pd.read_csv(infile, encoding = 'latin1', skiprows = line_breaks[j], nrows = line_breaks[j+interval] - line_breaks[j], names = datanames)
+
+        # Stop early if there's not enough speeches
+        if unit1.shape[0] < speech_count_thresh or unit2.shape[0] < speech_count_thresh:
+            continue
+
+        comp_unit1, comp_unit2 = None, None
+        if parties == ['N', 'N']:
+            comp_unit1 = unit1.loc[:, ['speech_id', 'speech']]
+            comp_unit2 = unit2.loc[:, ['speech_id', 'speech']]
+        else:
+            comp_unit1 = unit1.loc[(unit1.party == parties[0]), ['speech_id', 'speech']]
+            comp_unit2 = unit2.loc[(unit2.party == parties[1]), ['speech_id', 'speech']]
+
+        hc, features = None, None
+        try:
+            hc, features = two_unit_test(comp_unit1, comp_unit2, vocab)
+        except:
+            continue
+
+        # Write results to file
+        outfile = None
+        if cluster:
+            outfile = os.path.expanduser('~/Data/results_topics{}_{}_{}_{}.csv'.format(topic_list.shape[1]-2, interval, parties[0], parties[1]))
+        else:
+            outfile = '../Data/results_topics{}_{}_{}_{}.csv'.format(topic_list.shape[1]-2, interval, parties[0], parties[1])
         unit_dates = [str(unit1.date[2])[:6], str(unit2.date[2])[:6]]
         with open(outfile, 'a', newline = '') as csvfile:
             writer = csv.writer(csvfile)
--- Code/tukey_exp.py
+++ Code/tukey_exp.py
@@ -2,23 +2,30 @@ import os, sys, re, time, csv
 import numpy as np 
 import pandas as pd
 
-# sys.path.append('./DEP')
+sys.path.append('./DEP')
 import word_lists
 from exp_methods import *
 
 
 # Experimental parameters / setup
-# skiplines = 55
+skiplines = 48
 # line_breaks, dates = calculate_line_breaks(infile, skiplines)
-saved_breaks, saved_dates = word_lists.line_breaks, word_lists.dates
+saved_breaks, saved_dates = word_lists.line_breaks[skiplines:], word_lists.dates[skiplines:]
 nummonths = len(saved_dates)
-intervals = [1, 3, 6, 12]
 df = pd.read_csv(vocab_csv, encoding = 'latin1')
 vocab_list = list(df['word'])   # list of words to count
-params = build_params(intervals, nummonths)
+topic_25_list = pd.read_csv(topic_25_csv, encoding = 'latin1')
+topic_75_list = pd.read_csv(topic_75_csv, encoding = 'latin1')
+
+
+# intervals = [1, 3, 6, 12]   # intervals used for text
+# params = build_params(intervals, nummonths)
+intervals = [3, 12]         # intervals used for topics
+params = build_params(intervals, nummonths, [topic_25_list, topic_75_list])
 
 
 # Optimized for CJ parrun
 for k in range(len(params)):
     param = params[k]
-    run_experiment(infile, param[0], param[1], saved_breaks, saved_dates, vocab_list, param[2])
+    # run_text_experiment(infile, param[0], param[1], saved_breaks, saved_dates, vocab_list, param[2])
+    run_topic_experiment(infile, param[0], param[1], saved_breaks, saved_dates, param[3], param[2])
